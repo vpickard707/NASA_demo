f4f4d096e8123006cc165c101dfda1d7dd656fe3
vpickard707 <71297613+vpickard707@users.noreply.github.com>
2021-01-09 00:31:19 -0800

    added mysql routes plus axios

--- controller/controller.js
+++ controller/controller.js
@@ -3,22 +3,51 @@ const axios = require("axios");
 const db = require("../models");
 const key = process.env.bananakey;
 
+module.exports = {
+    picOfTheDay: function (req, res) {
+      axios
+        .get(`https://api.nasa.gov/planetary/apod?api_key=${key}`)
+        .then((response) => res.json(response.data))
+        .catch((err) => res.status(422).json(err));
+    },
+    findAllPost: function (req, res) {
+      db.Post.findAll({}).then(function (dbPost) {
+        res.json(dbPost).catch((err) => res.status(422).json(err));
+      });
+    },
+    createNewPost: function (req, res) {
+      db.Post.create({
+        title: req.body.title,
+        body: req.body.body,
+        create_at: req.body.created_at,
+      })
+        .then(function (dbPost) {
+          res.json(dbPost);
+        })
+        .catch((err) => res.status(422).json(err));
+    },
+  };
+
+// --------------------------------------------
 //mysql routes
 //Find All Posts
-module.exports = router;
-findAllPosts: function(req, res) {
-    db.Post.findAll({}).then(function (dbPost) {
-        res.json(dbPost).catch((err)) => res.status(422).json(err));
+// module.exports = router;
+// findAllPosts: function(req, res) {
+//     db.Post.findAll({}).then(function (dbPost) {
+//         res.json(dbPost).catch((err)) => res.status(422).json(err));
     
-    })
-},
-//createNewPost
+//     })
+// },
+// createNewPost
 // createNewPost: function (req, res) {
-    // db.Post
+//     db.Post.create({
+//         title: req.body.title,
+//         body: re
+//     })
 }
 //api routes from NASA
-picoftheday: function (req, res){
-    axios.get(`https://api.nasa.gov/planetary/apod?api_key=${key}`)
-    .then((res)) => res.json(res.data))
-    .catch((err)) = > res.status(422).json(err));
-}
+// picoftheday: function (req, res){
+//     axios.get(`https://api.nasa.gov/planetary/apod?api_key=${key}`)
+//     .then((res)) => res.json(res.data))
+//     .catch((err)) = > res.status(422).json(err));
+// }
