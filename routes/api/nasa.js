725f181929921bdc8c39cbfc98acbb535a357e65
vpickard707 <71297613+vpickard707@users.noreply.github.com>
2021-01-09 00:23:23 -0800

    fixed nasa api routes

--- routes/api/nasa.js
+++ routes/api/nasa.js
@@ -1,1 +1,7 @@
 const router = require("express").Router();
+const controller = require("../../controller/controller");
+
+// Matches with "/api/nasa"
+router.route("/").get(controller.picOfTheDay);
+
+module.exports = router;
