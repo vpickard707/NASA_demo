dc036dd2abf946e4eab492e8979523cf09934358
vpickard707 <71297613+vpickard707@users.noreply.github.com>
2021-01-09 00:19:56 -0800

    fixed syntax, routes for starting express app

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const express = require("express")
-const PORT = precess.env.PORT || 8080;
-const app = express ();
-
+const express = require("express");
+const PORT = process.env.PORT || 8080;
+const app = express();
+//API routes
 const routes = require("./routes");
 //models for sequelize
 const db = require("./models");
 
 //middleware for express
-app.use(express.urlencoded({ extended:true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //static directory
@@ -17,8 +17,9 @@ app.use(express.static("public"));
 app.use(routes);
 
 //Syncing our sequelize models and then starting our express app
-// db/sequelize.synce({force:true}).then(function){
-    // app.listen(PORT, () => {
-        // console.log(`world emoji == APY Server now on PORT ${PORT}`);
-    // })
-}
+//copied from Chris directly- where did he get this with world emoji?
+db.sequelize.sync({ force: true }).then(function () {
+  app.listen(PORT, () => {
+    console.log(`🌎 ==> API server now on port ${PORT} !`);
+  });
+});
