bba82bb71d1c919b49f0c0fe0b5cb24dfdd487d0
vpickard707 <71297613+vpickard707@users.noreply.github.com>
2021-01-09 00:56:03 -0800

    fixing errors

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,18 @@
+const path = require("path");
 const router = require("express").Router();
-const nasaRoutes = require("./nasa");
-const dbRoutes = require("./db");
-
+const apiRoutes = require("./api");
+// -----------------
+// const nasaRoutes = require("./nasa");
+// const dbRoutes = require("./db");
+//
 //Separated Routes
-router.use("/nasa", nasaRoutes);
-router.use("/posts", dbRoutes);
+// router.use("/nasa", nasaRoutes);
+// router.use("/posts", dbRoutes);
+// -----------
+// API Routes
+router.use("/api", apiRoutes);
+router.use("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "../public/main.html"));
+});
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
-const { builtinModules } = require("module");
+// // const { builtinModules } = require("module");
+// ----------------------------
 const path = require("path");
 const router = require("express").Router();
 const apiRoutes = require("./api");
 
 //API Routes
-router.use("api", apiRoutes);
+router.use("/api", apiRoutes);
 //mysql Routes
 router.use("/", function (req, res) {
-  res.sendFile(path.join(_dirname, "../public/main/html"));
+  res.sendFile(path.join(__dirname, "../public/main/html"));
 });
 module.exports = router;
